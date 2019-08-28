a940a5a53d5f47907e9d1c34dafcfb5195ada104
Merg3D <erik.vroon22@gmail.com>
2019-08-28 16:16:22 +0200

    Use 2d physics in jumper project

--- Titan Designer/assets/Scripts/coin.ts
+++ Titan Designer/assets/Scripts/coin.ts
@@ -3,6 +3,10 @@ extends Sprite
 define K_KEY Key("K")
 define L_KEY Key("L")
 
+func ready()
+	body = get_child("RigidBody2D")
+	body.set_as_box(true)
+
 func update()
 	if (Keyboard.is_key_pressed(K_KEY))
 		pos = pos - vec3(0.1, 0, 0)
--- Titan Designer/assets/Scripts/ground.ts
+++ Titan Designer/assets/Scripts/ground.ts
@@ -1,11 +1,5 @@
 extends Sprite
 
 func ready()
-	pos = vec2(70, -400)
-	size = vec2(300, 50)
-	bounds = vec4(0.0, 6.0, 0.0, 1.0)
-	
-	pc = PhysicsComponent()
-	physicscomponent = pc
-	pc.set_as_box(false)
-	
+	body = get_child("RigidBody2D")
+	body.set_as_box(false)
